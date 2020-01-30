1f236074014a3390baba477b27095a5a3b680280
Safna <safna.usa@gmail.com>
2020-01-29 22:40:05 -0600

    queries.sql all parts completed

--- queries.sql
+++ queries.sql
@@ -1,12 +1,17 @@
 ## Part 1: Test it with SQL
+CREATE TABLE job(
+id int PRIMARY KEY,
+employer VARCHAR(255),
+name VARCHAR(255),
+skills VARCHAR(255));
 
 ## Part 2: Test it with SQL
-select name from Employer where location = "Saint Louis"
+SELECT name F Employer WHERE location = "Saint Louis"
 
 ## Part 3: Test it with SQL
-Drop table job;
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
-select name,description from skill
-where skill.id in (select skills_id from job_skills
-where jobs_id in (select id from job));
+SELECT name,description FROM skill
+WHERE skill.id In (SELECT skills_id FROM job_skills
+WHERE jobs_id In (SELECT id FROM job));
