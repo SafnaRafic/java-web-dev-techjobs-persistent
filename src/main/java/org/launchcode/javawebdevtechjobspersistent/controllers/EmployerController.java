a83411f2e79175ce19f244320cff37d7520b125c
Safna <safna.usa@gmail.com>
2020-01-31 15:29:53 -0600

    Delete,Update Employer

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/EmployerController.java
@@ -11,11 +11,20 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.Optional;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
     @Autowired
     private EmployerRepository employerRepository;
+
+    @GetMapping("allEmployers")
+    public String displayAllEmployer(Model model) {
+        model.addAttribute("title", "All Employer");
+        model.addAttribute("employers", employerRepository.findAll());
+        return "employers/allEmployers";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -43,7 +52,54 @@ public class EmployerController {
             model.addAttribute("employer", employer);
             return "employers/view";
         } else {
-            return "redirect:../";
+            return "employers/allEmployers";
         }
+    }
+    @GetMapping("delete")
+    public String displayDeleteEmployerForm(Model model) {
+        model.addAttribute("title", "Delete Employer");
+        model.addAttribute("employers", employerRepository.findAll());
+        return "employers/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEmployerForm(@RequestParam(required = false) int[] employerId) {
+
+        if (employerId != null) {
+            for (int id : employerId) {
+                //employerRepository.delete(employerRepository.get(Employer.class,employerId));
+                employerRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping("update/{employerId}")
+    public String displayUpdateEmployerForm(Model model,@PathVariable int employerId) {
+        Optional employerToUpdate = employerRepository.findById(employerId);
+         if (employerToUpdate.isPresent()) {
+            Employer employer = (Employer) employerToUpdate.get();
+            model.addAttribute("employer", employer);
+            return "employers/update";
+        } else {
+            return "redirect:";
+        }
+
+    }
+
+    @PostMapping("update")
+    public String processUpdateEmployerForm(int employerId, String name, String location) {
+
+        Optional employerToUpdate = employerRepository.findById(employerId);
+        if (employerToUpdate.isPresent()) {
+            Employer employer = (Employer) employerToUpdate.get();
+            employer.setName(name);
+            employer.setLocation(location);
+            employerRepository.save(employer);
+        return "redirect:";
+        }
+
+        return "redirect:../";
     }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,10 +1,9 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +13,8 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Location is required")
     private String location;
 
-    @OneToMany(mappedBy = "employer")
+    @OneToMany(mappedBy = "employer", cascade = CascadeType.ALL)
+
    // @JoinColumn("blah")
     private final List<Job> jobs=new ArrayList<>();
 
